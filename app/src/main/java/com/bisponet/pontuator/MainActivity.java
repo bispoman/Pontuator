70bf3b1ff53d87ac72d5a6c522af544fc4c5d970
bispo <daniel@bisponet.com>
2018-02-02 23:44:19 -0200

    ajustes de layout, colocado o click nos botoes e mudada a fonte dos textos

--- app/src/main/java/com/bisponet/pontuator/MainActivity.java
+++ app/src/main/java/com/bisponet/pontuator/MainActivity.java
@@ -1,26 +1,43 @@
 package com.bisponet.pontuator;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.shapes.Shape;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Layout;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout linearLayout;
     Button b1, b2, b3, b5, b8, b13;
+    TextView displayingNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+// Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+//        ActionBar actionBar = getActionBar();
+//        actionBar.hide();
+
+        displayingNumber = (TextView)  findViewById(R.id.display);
         b1 = (Button) findViewById(R.id.btn_1);
         b2 = (Button) findViewById(R.id.btn_2);
         b3 = (Button) findViewById(R.id.btn_3);
@@ -28,29 +45,58 @@ public class MainActivity extends AppCompatActivity {
         b8 = (Button) findViewById(R.id.btn_8);
         b13 = (Button) findViewById(R.id.btn_13);
 
-        setButtonsBackground(this, this.getColor(R.color.btnStartColor),
-                this.getColor(R.color.btnEndColor));
+        Drawable btnBackground = getDrawable(R.drawable.btn_background);
+        b1.setBackground(btnBackground);
+        b2.setBackground(btnBackground);
+        b3.setBackground(btnBackground);
+        b5.setBackground(btnBackground);
+        b8.setBackground(btnBackground);
+        b13.setBackground(btnBackground);
 
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setNumber("1");
+            }
+        });
 
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setNumber("2");
+            }
+        });
 
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setNumber("3");
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setNumber("5");
+            }
+        });
+
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setNumber("8");
+            }
+        });
+
+        b13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setNumber("13");
+            }
+        });
     }
 
-    public void setButtonsBackground(Context context, int startcolor, int endcolor) {
-
-        int[] colors = new int[]
-                {ContextCompat.getColor(context, startcolor),
-                        ContextCompat.getColor(context, endcolor)};
-
-        GradientDrawable gradientDrawable =
-                new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, colors);
-        gradientDrawable.setCornerRadius(50);
-        gradientDrawable.setGradientType(GradientDrawable.LINEAR_GRADIENT);
-
-        b1.setBackground(gradientDrawable);
-        b2.setBackground(gradientDrawable);
-        b3.setBackground(gradientDrawable);
-        b5.setBackground(gradientDrawable);
-        b8.setBackground(gradientDrawable);
-        b13.setBackground(gradientDrawable);
+    private void setNumber(String number){
+        displayingNumber.setText(number);
     }
 }
